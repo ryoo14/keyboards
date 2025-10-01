4c61cb8805c0d8869633468321c7f5de3c632f1e
ryoo14 <anana12185@gmail.com>
2025-10-01 21:54:55 +0900

    add rhyl47

--- firmware/qmk/v0_2/keymaps/vial/config.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-#define VIAL_KEYBOARD_UID {0xE2, 0xB6, 0x78, 0x96, 0xA8, 0xFA, 0xFF, 0xBE}
-#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
-#define VIAL_UNLOCK_COMBO_COLS {0, 1}
---
+++ rhyn47/firmware/qmk/rules.mk
@@ -0,0 +1,2 @@
+# keyboards/rhyn47/rules.mk
+DEFAULT_FOLDER := rhyn47/v0_3
